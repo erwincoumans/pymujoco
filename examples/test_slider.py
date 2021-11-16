2c14840eb394c03dc2c5831e290638a1673490cf
Adwait Naik <adwaitnaik2@gmail.com>
2021-11-16 15:20:20 +0000

    slider sml script added with test

---
+++ examples/test_slider.py
@@ -0,0 +1,24 @@
+from pymujoco import *
+r = mjv_create_renderer()
+
+#help(p)
+m = mj_loadXML("slider.xml")
+#print(m.qpos0)
+#print(m.nq)
+#print(m.nv)
+d = mj_makeData(m)
+
+exit_requested=False
+while not exit_requested:
+  mj_step(m,d)
+  
+  print(d.qpos)
+  exit_requested = r.render(m,d)
+  
+names = ''.join([row.tostring().decode('UTF-8') for row in m.names]).split('\x00')
+print("names=",names)
+mj_printModel(m, "slider.txt")
+mj_deleteData(d)
+mj_deleteModel(m)
+
+mjv_delete_renderer(r)
